da6ea0fe8c1bd1a72a53312ba7d4c723a0394932
thimionii <mugolastbon@gmail.com>
2025-01-29 18:55:27 +0300

    active tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const[activetab, setActivetab] = useState('image-generator');
+
+  const handleTabChange = (tab) => {
+    alert(tab)
+    setActivetab(tab);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleTabChange('image-generator')}> Image Generator</button>
+      <button onClick={() => handleTabChange('chat')}> Chat</button>
+      <button onClick={() => handleTabChange('recipe-generator')}> Recipe Generator</button>
     </div>
   );
 }
