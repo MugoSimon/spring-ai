1be714a546d7213d0f18847718de8550b79763c0
thimionii <mugolastbon@gmail.com>
2025-01-29 19:17:03 +0300

    ui-appearance

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-
-  const[activetab, setActivetab] = useState('image-generator');
+  const [activetab, setActivetab] = useState("image-generator");
 
   const handleTabChange = (tab) => {
-    alert(tab)
+    //alert(tab);
     setActivetab(tab);
   };
 
-
   return (
     <div className="App">
-      <button onClick={() => handleTabChange('image-generator')}> Image Generator</button>
-      <button onClick={() => handleTabChange('chat')}> Chat</button>
-      <button onClick={() => handleTabChange('recipe-generator')}> Recipe Generator</button>
+      <button onClick={() => handleTabChange("image-generator")}>
+        Image Generator
+      </button>
+      <button onClick={() => handleTabChange("chat")}> Chat</button>
+      <button onClick={() => handleTabChange("recipe-generator")}>
+        Recipe Generator
+      </button>
+
+      <div>{activetab === "image-generator" && <h2>Image Generator</h2>}</div>
+      <div>{activetab === "chat" && <h2>Chat</h2>}</div>
+      <div>{activetab === "recipe-generator" && <h2>Recipe Generator</h2>}</div>
     </div>
   );
 }
